56dcebfec50b16fb2471aa71e84a9946281a09f6
ChrisGarozzo <garozzochristian@gmail.com>
2023-12-08 11:30:51 +0100

    added image calcutta with v-bind on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            message : "Hello Vue"
+    data() {
+        return {
+            message: "Hello Vue",
+            images: "https://media-assets.vanityfair.it/photos/614c90d5a7bbb646a22b4f2e/16:9/w_1920%2Cc_limit/calcutta-P.png"
         }
     }
 
